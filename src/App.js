570fffc825f87a76b4b71d64b74591fdcc7c6b6d
Kala Foods <guhan.delta@gmail.com>
2020-11-18 17:00:49 -0500

    Piped in some functions into the observable using pipe()

--- src/App.js
+++ src/App.js
@@ -12,13 +12,31 @@ import {
 import './App.css';
 
 // Creating an observable | Using the `$` is quite like a convention to say that it is an observable
-const observable$ = interval(1000); //interval()- emits an event as per the given milliseconds
+const observable$ = interval(1000).pipe( //interval()- emits an event as per the given milliseconds
+// pipe() - allows piping some pipeable operators to an observable, which do not change the existing- 
+// - Observable instance. Instead, they return a new Observable, whose subscription logic is based on the first Observable
+
+// RxJS deals with Sync and Async events, kindof in the same way, so if teh observable has it's own-
+//- inner state, it will return it synchronously 
+
+// Creating an initial state in the observable
+  startWith(10), //will start with 0 and waits for a second to emit the next value
+  scan(time => time - 1), //scan() is quite like reduce(), scan() shows all the values emitted, while-
+  //- reduce() given only the total
+  takeWhile(time => time > 0), // Emits values emitted by the source Observable so long as each value- 
+  //- satisfies the given condition, and then completes as soon as this condition is not satisfied.
+
+)
 
 function App() {
   const [ state, setState ] = useState();
   useEffect(()=>{
     // values emitted by the observable$ would be sent to the setState()
-    observable$.subscribe(setState);
+    const subscription = observable$.subscribe(setState);
+
+    return () => subscription.unsubscribe(); // The returned fn() unsubscribes from the subscription-
+    //- quite does the cleanup, like a cleanup function
+
   }, []); // If teh dependency array is not provided, the observer will unsubscribe from the observable-
   //- and subscribe to a new one, on every re-render | [] => not syncing with anything
   return (
